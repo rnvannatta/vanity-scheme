ce0f26751ab47603cda68aa889cfb8c9662b677e
rnvannatta <rnvannatta@gmail.com>
2025-07-02 09:26:41 -0400

    move test to proper folder

--- test/caselambda.scm
+++ test/caselambda.scm
@@ -6,7 +6,8 @@
     (case-lambda (() 0) ((x) 1) ((x y) 2)))
   (define (bar x) (foo 1 x)))
 (import (vanity core) (caselambda))
-(displayln (foo))
-(displayln (foo 1))
-(displayln (foo 2 3))
-(displayln (uwu (bar 99)))
+(exit (and
+(eq? (foo) 0)
+(eq? (foo 1) 1)
+(eq? (foo 2 3) 2)
+(eq? (uwu (bar 99)) 22)))
